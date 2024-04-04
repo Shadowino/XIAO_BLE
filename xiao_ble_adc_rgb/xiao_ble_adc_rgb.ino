b8a65085c283d8439510321469f3284482e0dd0b
shadowino <wasd29295@gmail.com>
2024-04-04 19:59:40 +0300

    add Servo

--- xiao_ble_adc_rgb/xiao_ble_adc_rgb.ino
+++ xiao_ble_adc_rgb/xiao_ble_adc_rgb.ino
@@ -1,4 +1,7 @@
 #include <Adafruit_TinyUSB.h> // for Serial
+#include <Servo.h>
+
+#define SERV_PORT 1
 
 
 #define REF_VLT 3.3
@@ -14,13 +17,14 @@
 #define REF_RNG 16384
 #endif
 
-#define REF_KEF REF_RNG/REF_VLT
+#define REF_KEF REF_VLT/(float)REF_RNG
 
 int i = 0;
 int ii = 0;
+Servo pntr;
 
 void setup() {
-
+  pntr.attach(SERV_PORT);
   // Set the resolution to 12-bit (0..4095)
   Serial.begin(115200);
   Serial.println("===========");
@@ -51,6 +55,7 @@ uint64_t tm1 = 9;
 int an = 0;
 char out[64];
 int pwm = 0;
+int ang = 0;
 void loop() {
   if (millis() - tm1 > 1000) {
     i++;
@@ -59,17 +64,16 @@ void loop() {
   ii = i % 3;
 //  int pwm = (i % 25) * 10;
   an = analogRead(A0);
-
+  ang = ((float)an / (float)REF_RNG) * 180;
   pwm = ((float)an / (float)REF_RNG) * 255;
 
-
-  sprintf(out, "I:%d\tA0:%d\tV:%f\tPWM:%d", i, an, an / REF_KEF, pwm);
+  sprintf(out, "I:%d\tA0:%d\tV:%f\tPWM:%d\tSERV:%d", i, an, an * (float)REF_KEF, pwm, ang);
   //  sprintf(out, " V:%f", (float)an / (float)REF_KEF);
   //Serial.print(i);
   Serial.println(out);
   //  delay(1000);                       // wait for a second
-
-  analogWrite(1, pwm);
+  pntr.write(ang);
+  analogWrite(2, pwm);
 
   digitalWrite(LED_RED, (ii == 0) ? 0 : 1);
   digitalWrite(LED_GREEN, (ii == 1) ? 0 : 1);
